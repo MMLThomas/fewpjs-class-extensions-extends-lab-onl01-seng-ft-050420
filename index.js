7dea7255e2b1bea19f6e5a999d5fb01ec71cf83a
michael thomas <michael.thomas2014@gmail.com>
2020-08-26 17:03:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ class Triangle extends Polygon{
 
 class Square extends Polygon{
   get area(){
-    return (this.perimeter/4)*(this.perimeter/4);
+    return Math.pow((this.perimeter/4),2);
   }
   
   get isValid(){
